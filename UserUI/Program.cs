396df45636f65df3655296837f3ff61311d55b84
Aldrin Kyle <Aldrin Kyle@KYLE-BOOK19>
2024-04-13 10:38:20 +0800

    Add project files.

---
+++ UserUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using UserAccountBusiness;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Username: ");
+        string UserName = Console.ReadLine();
+
+        Console.WriteLine("Enter Password: ");
+        string password = Console.ReadLine();
+
+        UserService userService = new UserService();
+        bool result = userService.VerifyUser(UserName, password);
+
+        if (result)
+        {
+            Console.WriteLine("Welcome to UserUI");
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+}
